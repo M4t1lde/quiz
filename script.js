e05e26b1a5a2f1ab059e88baf67fb69315228de8
Matilde <matildejb@outlook.com>
2025-03-26 13:33:39 +0100

    farge på svar

--- script.js
+++ script.js
@@ -6,9 +6,33 @@ function calculateScore() {
     questions.forEach((q, index) => {
         let selected = q.querySelector("input[name='q" + (index + 1) + "']:checked");
         if (selected) {
-            sluttpoeng += parseInt(selected.value);
-            if (selected.value === "0") {
-                selected.style.color = "#ec7063";  // feil svar er røde
+            sluttpoeng += parseInt(selected.value); //plusser på poeng(values) fra alle spørsmålene
+            if (index === 0 && parseInt(selected.value) === 0){ //spørsmål, rød=feil, grønn=riktig
+                document.getElementById("h2").style.color = "#ff0000";
+            }
+            else if (index === 0 && parseInt(selected.value) === 1){
+                document.getElementById("h2").style.color = "#006400";
+            }
+
+            if (index === 1 && parseInt(selected.value) === 0){
+                document.getElementById("h2.1").style.color = "#ff0000";
+            }
+            else if (index === 1 && parseInt(selected.value) === 1){
+                document.getElementById("h2.1").style.color = "#006400";
+            }
+
+            if (index === 2 && parseInt(selected.value) === 0){
+                document.getElementById("h2.2").style.color = "#ff0000";
+            }
+            else if (index === 2 && parseInt(selected.value) === 1){
+                document.getElementById("h2.2").style.color = "#006400";
+            }
+
+            if (index === 3 && parseInt(selected.value) === 0){
+                document.getElementById("h2.3").style.color = "#ff0000";
+            }
+            else if (index === 3 && parseInt(selected.value) === 1){
+                document.getElementById("h2.3").style.color = "#006400";
             }
         }
     }); //hver ".boks" er +1 index. bare det riktige har en value på 1.
