17e8e09bc6b7d426d9eff92e304d4894703d8678
Matilde <matildejb@outlook.com>
2025-03-26 12:48:00 +0100

    bedre skrift

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ function calculateScore() {
         let selected = q.querySelector("input[name='q" + (index + 1) + "']:checked");
         if (selected) {
             sluttpoeng += parseInt(selected.value);
+            if (selected.value === "0") {
+                selected.style.color = "#ec7063";  // feil svar er røde
+            }
         }
     }); //hver ".boks" er +1 index. bare det riktige har en value på 1.
-    
     document.getElementById("score").innerText = "Poeng: " + sluttpoeng; //regner sluttscore
-    document.getElementById("tryagain").innerText = "Prøv igjen";
-    document.getElementById("boks").style.color = "#ff0000";
+    document.getElementById("tryagain").innerText = "Prøv igjen"; //starter siden på nytt
+    window.scrollTo(0, document.body.scrollHeight); //skroller ned hele siden til poeng. kilde:(https://stackoverflow.com/questions/11715646/scroll-automatically-to-the-bottom-of-the-page)
+    
 }
 
 function Klikk() {
